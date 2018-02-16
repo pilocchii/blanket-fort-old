75be7ea8fa18bb0e6e24bd51d4a6482f93182b44
DevonS3 <32690987+DevonS3@users.noreply.github.com>
2018-02-16 15:13:07 -0800

    remove duplicate lines in core

--- core.js
+++ core.js
@@ -78,14 +78,6 @@ define([
         // }
         // mapreader.readAsText("maps/testmap");
 
-        let levelOne = new LevelOne(gameEngine, ASSET_MANAGER, ctx);
-
-        // let mapreader = new FileReader();
-        // mapreader.onload = function(e) {
-        //     console.log(mapreader.result);
-        // }
-        // mapreader.readAsText("maps/testmap");
-
         // gameEngine.showOutlines = true;
 
         // let gameboard = new GameBoard();
