37d10413b84ec45c6ad8e14f5f2dd6e386aa97b6
DevonS3 <32690987+DevonS3@users.noreply.github.com>
2018-03-03 22:32:47 -0800

    Fireball jumps out of lava

--- entities/hazards.js
+++ entities/hazards.js
@@ -23,6 +23,9 @@ define([
                 this.boundX = this.x - this.spriteWidth;
                 this.boundY = this.y - this.spriteHeight * this.scale + 37 * this.scale;
 
+                this.fireCooldownTimer = 0;
+                this.fireCooldown = 100;
+
                 this.states = {
                     "active": true,
                     "facingRight": true,
@@ -35,7 +38,13 @@ define([
 
             /*Updates the entity each game loop. i.e. what does this entity do? */
             update() {
-                super.update();
+                if (Math.abs(this.x - this.game.hero.x) <= 500 && this.fireCooldownTimer <= 0) {
+                    this.game.addEntity(new Fireball(this.game, this.x - 32, this.y - this.spriteHeight*2, this.img, this.ctx, 4, 15));
+                    this.fireCooldownTimer = this.fireCooldown;
+                }
+                if (this.fireCooldownTimer > 0) {
+                    this.fireCooldownTimer--;
+                }
             }
 
             drawOutline(ctx) {
@@ -54,27 +63,29 @@ define([
             }
 
             drawImg(ctx) {
-
                 this.animation.drawFrame(1, ctx, this.x, this.y, this.states.facingRight);
                 this.drawOutline(ctx);
             }
         }
 
         class Fireball extends Entity {
-            constructor(game, x, y, img = null, ctx = null, scale = null) {
+            constructor(game, x, y, img = null, ctx = null, scale = null, ySpeed = 12) {
                 super(game, x, y, img, ctx);
                 this.scale = scale;
                 this.spriteWidth = 60;
                 this.spriteHeight = 60;
                 this.centerX = x + ((this.spriteWidth * this.scale) / 2) - this.spriteWidth;
                 this.boundWidth = 6 * this.scale;
-                this.boundHeight = 3*this.scale;
+                this.boundHeight = 20 * this.scale;
                 this.boundX = this.centerX - this.boundWidth/2;
-                this.boundY = this.y + this.spriteHeight * this.scale/2 - this.boundHeight;
+                this.boundY = this.y - this.spriteHeight*this.scale/2;
+
+                this.ySpeed = ySpeed;
+                this.damage = 2;
 
                 this.states = {
-                    "active": true,
-                    "start": false,
+                    "active": false,//TODO: Determine if we want this state. (almost definitely unneeded)
+                    "start": true,
                     "middle_up": false,
                     "peak_up": false,
                     "peak_down": false,
@@ -83,14 +94,75 @@ define([
                     "facingRight": true,
                 };
                 this.animations = {
-                    "active": new Animation(this.img, [this.spriteWidth, this.spriteHeight], 9, 13, 5, 6, true, this.scale, 6),
+                    "active": new Animation(this.img, [this.spriteWidth, this.spriteHeight], 9, 13, 3, 6, true, this.scale, 6),
+                    "start": new Animation(this.img, [this.spriteWidth, this.spriteHeight], 9, 13, 3, 1, true, this.scale, 6),
+                    "middle_up": new Animation(this.img, [this.spriteWidth, this.spriteHeight], 9, 13, 3, 1, true, this.scale, 7),
+                    "peak_up": new Animation(this.img, [this.spriteWidth, this.spriteHeight], 9, 13, 3, 1, true, this.scale, 8),
+                    "peak_down": new Animation(this.img, [this.spriteWidth, this.spriteHeight], 9, 13, 3, 1, true, this.scale, 9),
+                    "middle_down": new Animation(this.img, [this.spriteWidth, this.spriteHeight], 9, 13, 3, 1, true, this.scale, 10),
+                    "finish": new Animation(this.img, [this.spriteWidth, this.spriteHeight], 9, 13, 3, 1, true, this.scale, 11),
                 };
                 this.animation = this.animations.active;
+                console.log(this.gravity);
             }
 
             /*Updates the entity each game loop. i.e. what does this entity do? */
             update() {
-                super.update();
+                if (this.states.start) {
+                    this.changePos(0, -1*this.ySpeed);
+                    if (this.animation.loops > 4) {
+                        this.animation.elapsedTime = 0;
+                        this.animation.loops = 0;
+                        this.states.start = false;
+                        this.states.middle_up = true;
+                    }
+                }
+                if (this.states.middle_up) {
+                    this.changePos(0, -.5*this.ySpeed);
+                    if (this.animation.loops > 3) {
+                        this.animation.elapsedTime = 0;
+                        this.animation.loops = 0;
+                        this.states.middle_up = false;
+                        this.states.peak_up = true;
+                    }
+                }
+                if (this.states.peak_up) {
+                    this.changePos(0, -.1*this.ySpeed);
+                    if (this.animation.loops > 2) {
+                        this.animation.elapsedTime = 0;
+                        this.animation.loops = 0;
+                        this.states.peak_up = false;
+                        this.states.peak_down = true;
+                    }
+                }
+                if (this.states.peak_down) {
+                    this.changePos(0, .1*this.ySpeed);
+                    if (this.animation.loops > 2) {
+                        this.animation.elapsedTime = 0;
+                        this.animation.loops = 0;
+                        this.states.peak_down = false;
+                        this.states.middle_down = true;
+                    }
+                }
+                if (this.states.middle_down) {
+                    this.changePos(0, .5*this.ySpeed);
+                    if (this.animation.loops > 3) {
+                        this.animation.elapsedTime = 0;
+                        this.animation.loops = 0;
+                        this.states.middle_down = false;
+                        this.states.finish = true;
+                    }
+                }
+                if (this.states.finish) {
+                    this.changePos(0, this.ySpeed);
+                    if (this.animation.loops > 4) {
+                        this.animation.elapsedTime = 0;
+                        this.animation.loops = 0;
+                        this.states.finish = false;
+                        this.states.start = true;
+                        this.removeFromWorld = true;
+                    }
+                }
             }
 
             drawOutline(ctx) {
@@ -104,12 +176,31 @@ define([
             }
 
             draw(ctx) {
-                if (this.states.active)
-                    this.drawImg(ctx);
+                if (this.states.active) {
+                    //
+                }
+                if (this.states.start) {
+                    this.animation = this.animations.start;
+                }
+                if (this.states.middle_up) {
+                    this.animation = this.animations.middle_up;
+                }
+                if (this.states.peak_up) {
+                    this.animation = this.animations.peak_up;
+                }
+                if (this.states.peak_down) {
+                    this.animation = this.animations.peak_down;
+                }
+                if (this.states.middle_down) {
+                    this.animation = this.animations.middle_down;
+                }
+                if (this.states.finish) {
+                    this.animation = this.animations.finish;
+                }
+                this.drawImg(ctx);
             }
 
             drawImg(ctx) {
-
                 this.animation.drawFrame(1, ctx, this.x, this.y, this.states.facingRight);
                 this.drawOutline(ctx);
             }
